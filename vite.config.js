0fa9ee078661c0be2fa1ec8c5b4eda938bc2052f
CJW23 <cjw7242@gmail.com>
2023-01-24 02:04:33 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/User-Manage-System/",
   plugins: [vue()],
   resolve: {
     alias: {
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  publicPath: '/User-Manage-System',
-  outputDir: 'docs'
-}
